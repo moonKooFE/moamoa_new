f3bac8390a9208bebbac3586b085fdb8864f24ae
jayou2323 <jayou1223@gmail.com>
2023-11-22 21:52:33 +0900

    메인화면 좌우 스크롤 구현

--- src/Components/Function/sessionExpiration.js
+++ src/Components/Function/sessionExpiration.js
@@ -1,29 +1,32 @@
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import client from "../../Client";
 
 const SessionExpiration = () => {
-    const navigate = useNavigate();
 
     useEffect(()=>{
+        console.log(sessionStorage.getItem('token'));
         client.defaults.headers.common['Authorization'] = sessionStorage.getItem('token');
         client.get('/users?nickname=asdf')
         .then(function(res) {
           //console.log("로그인 인증 성공");
         }).catch(function(err){
           if(err.response.status == "401"){
-            alert("로그인 세션이 만료되었습니다. 다시 로그인 해주세요.");
-            sessionStorage.clear();
-            client.defaults.headers.common['Authorization'] = undefined;
-            sessionStorage.setItem('isLogin', false);
-            navigate('/onBoard');
+            SessionExpirationAction();
           } else {
             alert("서버 연결이 불안정 합니다. 재접속을 시도해 주세요.");
-            navigate('/Random');
+            window.location.replace('/Random');
           }
           //console.log(err);
         });
       }, []);
+}
+
+const SessionExpirationAction = () => {
+  alert("로그인 세션이 만료되었습니다. 다시 로그인 해주세요.");
+  sessionStorage.clear();
+  client.defaults.headers.common['Authorization'] = undefined;
+  sessionStorage.setItem('isLogin', false);
+  window.location.replace('/onBoard');
 }
 
 export default SessionExpiration;
